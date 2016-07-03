269920933b2603acdd85b40d911ea2469ca976a1
Sergei Dauletau <sergei.dauletau@onesource.com>
2016-07-03 09:00:12 -0500

    elasticsearch 2.3.3

--- scripts/install-plugin.sh
+++ scripts/install-plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/opt/elasticsearch-2.1.1/bin/plugin install file:./target/releases/elasticsearch-simple-similarity-2.1.1.zip
+/usr/local/opt/elasticsearch-2.3.3/bin/plugin install file:./target/releases/elasticsearch-simple-similarity-2.3.3.zip
