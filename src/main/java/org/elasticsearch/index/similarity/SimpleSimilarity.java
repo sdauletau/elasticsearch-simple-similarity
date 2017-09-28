8e4386b802c235c191c779b0f65fcf2b2ce548af
sdauletau <sdauletau@gmail.com>
2017-09-27 20:17:58 -0500

    elasticsearch 6.0.0-beta2

--- src/main/java/org/elasticsearch/index/similarity/SimpleSimilarity.java
+++ src/main/java/org/elasticsearch/index/similarity/SimpleSimilarity.java
@@ -37,137 +37,78 @@ public class SimpleSimilarity extends Similarity {
     }
 
     public final SimWeight computeWeight(float boost, CollectionStatistics collectionStats, TermStatistics... termStats) {
-        final Explanation idf = termStats.length == 1
-                ? idfExplain(collectionStats, termStats[0])
-                : idfExplain(collectionStats, termStats);
-        return new IDFStats(idf);
-    }
-
-    public final SimScorer simScorer(SimWeight stats, LeafReaderContext context) throws IOException {
-        IDFStats idfstats = (IDFStats) stats;
-        return new SimpleTFIDFSimScorer(idfstats);
-    }
-
-    private float sloppyFreq(int distance) {
-        return 1.0f / (distance + 1);
-    }
-
-    private float scorePayload(int doc, int start, int end, BytesRef payload) {
-        return 1.0f;
-    }
-
-    private float tf(float freq) {
-        return 1.0f;
-    }
-
-    private float idf(long docFreq, long numDocs) {
-        return 1.0f;
-    }
-
-    private Explanation idfExplain(CollectionStatistics collectionStats, TermStatistics termStats) {
-        final long df = termStats.docFreq();
-        final long max = collectionStats.maxDoc();
-        final float idf = idf(df, max);
-        return Explanation.match(idf, "idf(docFreq=" + df + ", maxDocs=" + max + ")");
-    }
-
-    private Explanation idfExplain(CollectionStatistics collectionStats, TermStatistics termStats[]) {
-        final long max = collectionStats.maxDoc();
-        float idf = 0.0f;
-        List<Explanation> subs = new ArrayList<>();
-        for (final TermStatistics stat : termStats) {
-            final long df = stat.docFreq();
-            final float termIdf = idf(df, max);
-            subs.add(Explanation.match(termIdf, "idf(docFreq=" + df + ", maxDocs=" + max + ")"));
-            idf += termIdf;
-        }
-        return Explanation.match(idf, "idf(), sum of:", subs);
-    }
-
-    private final class SimpleTFIDFSimScorer extends SimScorer {
-        private final IDFStats stats;
-        private final float weightValue;
-
-        SimpleTFIDFSimScorer(IDFStats stats) throws IOException {
-            this.stats = stats;
-            this.weightValue = stats.boost;
-        }
-
-        public float score(int doc, float freq) {
-            return tf(freq) * weightValue;
-        }
-
-        public float computeSlopFactor(int distance) {
-            return sloppyFreq(distance);
-        }
-
-        public float computePayloadFactor(int doc, int start, int end, BytesRef payload) {
-            return scorePayload(doc, start, end, payload);
-        }
-
-        public Explanation explain(int doc, Explanation freq) {
-            return explainScore(doc, freq, stats);
+        if (termStats.length == 1) {
+            return new SimpleSimilarityWeight(boost, collectionStats, termStats[0]);
+        } else {
+            return new SimpleSimilarityWeight(boost, collectionStats, termStats);
         }
     }
 
-    private static class IDFStats extends SimWeight {
-        private final Explanation idf;
+    private static class SimpleSimilarityWeight extends SimWeight {
+        private final Explanation score;
         private float boost;
 
-        IDFStats(Explanation idf) {
-            this.idf = idf;
+        SimpleSimilarityWeight(float boost, CollectionStatistics collectionStats, TermStatistics termStats) {
+            this.score = Explanation.match(1.0f, "term score");
+            this.boost = boost;
         }
 
-        /** The value for normalization of contained query clauses (e.g. sum of squared weights).
-         * <p>
-         * NOTE: a Similarity implementation might not use any query normalization at all,
-         * it's not required. However, if it wants to participate in query normalization,
-         * it can return a value here.
-         */
-        public float getValueForNormalization() {
-            // do not use any query normalization
-            return 1.0f;
-        }
-
-        /** Assigns the query normalization factor and boost from parent queries to this.
-         * <p>
-         * NOTE: a Similarity implementation might not use this normalized value at all,
-         * it's not required. However, it's usually a good idea to at least incorporate
-         * the topLevelBoost (e.g. from an outer BooleanQuery) into its score.
-         */
-        public void normalize(float queryNorm, float boost) {
+        SimpleSimilarityWeight(float boost, CollectionStatistics collectionStats, TermStatistics termStats[]) {
+            float total = 0.0f;
+            List<Explanation> scores = new ArrayList<>();
+            for (final TermStatistics stat : termStats) {
+                scores.add(Explanation.match(1.0f, "term score"));
+                total += 1.0f;
+            }
+            this.score = Explanation.match(total, "total terms score, sum of:", scores);
             this.boost = boost;
         }
     }
 
-    private Explanation explainQuery(IDFStats stats) {
-        List<Explanation> explanations = new ArrayList<>();
-
-        Explanation boostExpl = Explanation.match(stats.boost, "boost");
-        if (stats.boost != 1.0f)
-            explanations.add(boostExpl);
-        explanations.add(stats.idf);
-
-        return Explanation.match(
-                boostExpl.getValue() * stats.idf.getValue(),
-                "queryWeight, product of:", explanations);
+    public final SimScorer simScorer(SimWeight weight, LeafReaderContext context) throws IOException {
+        SimpleSimilarityWeight simpleSimilarityWeight = (SimpleSimilarityWeight) weight;
+        return new SimpleSimilarityScorer(simpleSimilarityWeight);
     }
 
-    private Explanation explainField(int doc, Explanation freq, IDFStats stats) {
-        Explanation tfExplanation = Explanation.match(tf(freq.getValue()), "tf(freq="+freq.getValue()+"), with freq of:", freq);
+    private final class SimpleSimilarityScorer extends SimScorer {
+        private final SimpleSimilarityWeight weight;
 
-        return Explanation.match(
-                tfExplanation.getValue() * stats.idf.getValue(),
-                "fieldWeight in " + doc + ", product of:",
-                tfExplanation, stats.idf);
-    }
+        SimpleSimilarityScorer(SimpleSimilarityWeight weight) throws IOException {
+            this.weight = weight;
+        }
 
-    private Explanation explainScore(int doc, Explanation freq, IDFStats stats) {
-        Explanation queryExpl = explainQuery(stats);
-        Explanation fieldExpl = explainField(doc, freq, stats);
-        return Explanation.match(
-                queryExpl.getValue() * fieldExpl.getValue(),
-                "score(doc="+doc+", freq="+freq.getValue()+"), product of:",
-                queryExpl, fieldExpl);
+        public float score(int doc, float freq) {
+            return weight.boost;
+        }
+
+        public float computeSlopFactor(int distance) {
+            return 1.0f / (distance + 1);
+        }
+
+        public float computePayloadFactor(int doc, int start, int end, BytesRef payload) {
+            return 1.0f;
+        }
+
+        public Explanation explain(int doc, Explanation freq) {
+            return explainScore(doc, freq, weight);
+        }
+
+        private Explanation explainQuery(SimpleSimilarityWeight weight) {
+            Explanation boost = Explanation.match(weight.boost, "boost");
+            return Explanation.match(boost.getValue(),"query score, result of:", boost);
+        }
+
+        private Explanation explainField(SimpleSimilarityWeight weight) {
+            return Explanation.match(weight.score.getValue(),"field score, result of:", weight.score);
+        }
+
+        private Explanation explainScore(int doc, Explanation freq, SimpleSimilarityWeight weight) {
+            Explanation queryExpl = explainQuery(weight);
+            Explanation fieldExpl = explainField(weight);
+            return Explanation.match(
+                    queryExpl.getValue() * fieldExpl.getValue(),
+                    "score, product of:",
+                    queryExpl, fieldExpl);
+        }
     }
 }
